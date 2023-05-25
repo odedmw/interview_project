93706df9f6e050863222d0652d048ba87eb898ce
odedmw <120259277+odedmw@users.noreply.github.com>
2023-05-26 02:10:12 +0300

    Second Update code.py

--- requirements_str/code.py
+++ requirements_str/code.py
@@ -1,20 +1,21 @@
-def get_requirements(file_path, list_passed_files=[], str_modules=""):
+def get_requirements(file_path, list_passed_files=[], str_modules="", list_modules=[]):
     with open(file_path, "r") as file:
         for module in file:
             if "-r " != module[:3]:
-                
-                if len(str_modules) != 0:
-                    str_modules += "," + module.split("==")[0]
-                else:
-                    str_modules += module.split("==")[0]
+                if module.split("==")[0] not in list_modules:
+                    if len(str_modules) != 0:
+                        str_modules += "," + module.split("==")[0]
+                        list_modules.append(module.split("==")[0])
+                    else:
+                        list_modules.append(module.split("==")[0])
+                        str_modules += module.split("==")[0]
             else:
                 file_name = module.split(" ")[1].split(".")[0]
-                
                 if "requirements" in file_name and file_name not in list_passed_files:
                     list_passed_files.append(file_name)
                     new_path = file_path.split("\\")
                     new_path[-1] = module.split(" ")[1].strip("\n")
                     new_path = "/".join(new_path)
-                    str_modules = get_requirements(new_path, list_passed_files, str_modules)
+                    str_modules = get_requirements(new_path, list_passed_files, str_modules, list_modules)
 
     return str_modules
